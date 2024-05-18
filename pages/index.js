9c4bc67d3a3e64a0f73b13b82448bfbb988e7236
Shreekar11 <shreekargade2004@gmail.com>
2024-05-18 23:42:59 +0530

    navbar made fixed

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Posts from '../components/Posts';
 import Footer from '../components/Footer';
 import TopNavbar from '../components/Navbar';
 
 export default function HomePage() {
+  const [isNavbarFixed, setIsNavbarFixed] = useState(false);
+
+  const handleScroll = () => {
+    const fixedScrollY = 100;
+    if (window.scrollY > fixedScrollY) {
+      setIsNavbarFixed(true);
+    } else {
+      setIsNavbarFixed(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div
       style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}
     >
-      <TopNavbar />
       <div
-        style={{
-          margin: '60px 0px 20px',
-        }}
+        style={
+          isNavbarFixed
+            ? { position: 'fixed', top: 0, width: '100%', zIndex: 1000 }
+            : null
+        }
       >
-        <Posts />
+        <TopNavbar />
       </div>
-      <Footer />
+      <div style={{ marginTop: isNavbarFixed ? '60px' : '0px' }}>
+        <div style={{ margin: '60px 0px 20px' }}>
+          <Posts />
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
